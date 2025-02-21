07447faee1acbefd375424fa73c1eedd7d272abb
Dante Alves <dantealves8@gmail.com>
2025-02-20 22:28:55 -0300

    create synthax.py

---
+++ Python/synthax.py
@@ -0,0 +1,17 @@
+# comentário de 1 linha
+"""Documentação com três aspas"""
+
+# divisão de piso é com //
+    # 7 // 2 == 3 
+    # -7 // 2 == -4 ---> pois a divisão de piso considera o maior número inteiro que seja menor do que a divisão real do valor (que no caso é 3.5)
+
+# exponenciação é com **
+
+#booleanos são tipos primitivos
+    #True == not False
+    #False == not True => pode usar o not para inverter o significado
+
+var1 = 5 + 3
+
+print("Hello World!")
+print("Testando", var1)
